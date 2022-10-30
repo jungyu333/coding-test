717d989224d4e3c1031fc559e2a3668829db7444
jungyu <jungyu@naver.com>
2022-10-30 21:16:59 +0900

    solution

---
+++ 1822.Sign of the Product of an Array/index.js
@@ -0,0 +1,16 @@
+const arraySign = function (nums) {
+  let product = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) return 0;
+    product *= nums[i];
+  }
+
+  if (product === 0) {
+    return 0;
+  } else if (product > 0) {
+    return 1;
+  } else {
+    return -1;
+  }
+};
