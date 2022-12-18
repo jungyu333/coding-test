b407d51f9d0d065b9196f70495b22b4899c9dbd7
jungyu333 <jungyu3826@naver.com>
2022-12-18 23:10:13 +0900

    solution

--- 1588.Sum of All Odd Length Subarrays/index.js
+++ 1588.Sum of All Odd Length Subarrays/index.js
@@ -1,9 +1,11 @@
-var sumOddLengthSubarrays = function (arr) {
+const sumOddLengthSubarrays = function (arr) {
   let sum = 0;
   for (let i = 1; i <= arr.length; i += 2) {
+    // 홀수 개씩 subarray
     for (let j = 0; j <= arr.length - i; j++) {
+      //배열 앞에서부터 홀수개씩 배열을 자른다
       let temp = arr.slice(j, i + j);
-      sum += temp.reduce((sum, item) => sum + item);
+      sum += temp.reduce((sum, item) => sum + item); // 배열의 합을 sum에 더해준다
     }
   }
 
